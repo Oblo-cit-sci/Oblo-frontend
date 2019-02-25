1d24db8e906f1306a2e7d25478b5ccda7d1cd977
Ramin S <Ramin.Soleymani@uab.cat>
2019-02-25 13:10:15 +0100

    works with npm run build; npm run start

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card>
+        <Subscription></Subscription>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Subscription from '~/components/Subscription.vue'
+
+
+export default {
+  components: {
+    Subscription
+  }
+}
+</script>
