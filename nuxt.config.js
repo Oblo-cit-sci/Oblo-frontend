6dd8a64ca0d0ae22f750b64bfaac2ad1b463be20
Ramin S <Ramin.Soleymani@uab.cat>
2019-03-05 13:40:04 +0100

    basic element-ui as plugin works. now formschema...

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,15 +35,17 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    '~/assets/style/app.styl'
+    '~/assets/style/app.styl',
+    'element-ui/lib/theme-chalk/index.css'
   ],
 
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/vuetify'
-  ],
+    '@/plugins/vuetify',
+    '~plugins/element-ui'
+],
 
   /*
   ** Nuxt.js modules
--- pages/CreateEntry.vue
+++ pages/CreateEntry.vue
@@ -1,13 +1,36 @@
 <template>
-    
+  <div>
+    <div>
+      <el-button type="primary">{{ui_text.site}}</el-button>
+      <el-button type="primary">{{ui_text.village}}</el-button>
+    </div>
+    <br>
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item :title="ui_text.village" name="1">
+        <el-button type="primary">{{ui_text.village_info_card}}</el-button>
+        <el-button type="primary">{{ui_text.focus_group}}</el-button>
+      </el-collapse-item>
+      </el-collapse>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "CreateEntry"
+        name: "CreateEntry",
+        data() {
+          return {
+            ui_text: {
+              "site": "Site",
+              "village": "Village",
+              "village_info_card": "Village Information Card",
+              "focus_group": "Focus Group Discussion"
+          }
+          }
+        }
     }
 </script>
 
 <style scoped>
+
 
 </style>
