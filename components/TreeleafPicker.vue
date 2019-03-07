8df26ca188a2f4e66760ffdd8d3815ee5eae5d1a
Ramin S <Ramin.Soleymani@uab.cat>
2019-03-07 16:17:48 +0100

    licci code selector. refines

--- components/TreeleafPicker.vue
+++ components/TreeleafPicker.vue
@@ -8,6 +8,7 @@
             v-btn(@click="remove(treelevel)")
               v-icon block
       v-btn(v-for="(option, index) in act_options" :key="option.code" color="info" @click="select(index)") {{option.name}}
+      div(v-if="selected_code !== 0") {{selected_code}}
 </template>
 
 <script>
@@ -21,19 +22,23 @@
       return {
         tree: licci_tree,
         selection: [], // indices of children
-        act_options: licci_tree.children
+        act_options: licci_tree.children,
+        selected_code: 0
       }
     },
     methods: {
       select: function (index) {
         this.selection.push([index, this.act_options[index]]);
+        this.selected_code = this.act_options[index].code;
         this.act_options = this.act_options[index].children;
       },
       remove: function (treelevel) {
         this.selection = this.selection.slice(0,treelevel);
+        this.selected_code = 0;
         let options = licci_tree.children;
         for(let node of this.selection) {
             options = options[node[0]].children;
+            this.selected_code = options[node[0]].code;
         }
         this.act_options = options;
       }
