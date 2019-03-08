d8fdaedf53f0e2ec30bafdd8744526824d8e261d
Ramin <transfluxus@posteo.de>
2019-03-08 14:14:40 +0100

    profile. begining of edit

---
+++ components/Taglist.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div
+    v-chip(v-for="tag in tags" :key="tag.name") {{tag.name}}
+      v-icon(v-if="tag.hasOwnProperty('icon')") {{tag.icon}}
+</template>
+
+<script>
+  export default {
+    name: "Taglist",
+    props: ['tags']
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/actor/_profile.vue
+++ pages/actor/_profile.vue
@@ -1,49 +1,63 @@
 <template lang="pug">
   v-flex(xs12,sm8,md6)
 
-    v-text-field(v-if="edit_mode", "outline", label="public name")
-    div(v-else) {{publicName}}
+    v-text-field(v-if="edit_mode", "outline", label="public name" v-model="edits.public_name")
+    div(v-else) {{profile.public_name}}
     v-list
       v-subheader General
 
       v-list-tile
         v-list-tile-content
-          v-list-tile-title @{{name}}
+          v-list-tile-title @{{profile.username}}
           v-list-tile-sub-title username
 
-      v-textarea(v-if="edit_mode", "outline", label="description")
+      v-textarea(v-if="edit_mode", "outline", label="description" v-model="edits.description")
       v-list-tile(v-else)
         v-list-tile-content
-          v-list-tile-title {{description}}
+          v-list-tile-title {{profile.description}}
           v-list-tile-sub-title Description
 
-      v-text-field(v-if="edit_mode", "outline", label="location")
+      v-text-field(v-if="edit_mode", "outline", label="location"  v-model="edits.location")
       v-list-tile(v-else)
         v-list-tile-content
-          v-list-tile-title {{location}}
+          v-list-tile-title {{profile.location}}
           v-list-tile-sub-title Location
 
       v-divider
       v-subheader Interested topics
-
-      v-chip(v-for="topic in interested_topics" :key="topic") {{topic}}
-        v-icon star
+      Taglist(:tags="profile.interested_topics")
 
       v-divider
 
-      v-tabs(v-model="selected_tab")
+      v-tabs(v-model="selected_tab" v-if="!edit_mode")
         v-tab Entries
         v-tab follows
         v-tab following
         v-tab Groups
+        v-tab Roles
 
     div(v-if="personal")
-      v-btn(color="info" @click="setEdit") Edit
+
+      v-btn(v-if="!edit_mode" color="info" @click="setEdit") Edit
+      div(v-else)
+        v-btn(color="warning" @click="cancelEdit") Cancel
+        v-btn(color="success" @click="doneEdit") Save
+
 </template>
+
 <script>
+
+  import Taglist from "../../components/Taglist.vue"
+
+  let editable = [
+    "public_name", "description", "location", "interested_topics"
+  ];
 
   export default {
     name: "profile",
+    components: {
+      Taglist
+    },
     validate({params}) {
       // Must be a number
       // console.log(params.profile);
@@ -58,7 +72,20 @@
         // console.log(this.$root._route.params.profile);
       },
       setEdit: function () {
-        this.edit_mode = !this.edit_mode;
+        this.edit_mode = true;
+
+        for(let value of editable) {
+          this.edits[value] = this.profile[value];
+        }
+      },
+      cancelEdit: function () {
+        this.edit_mode = false;
+      },
+      doneEdit: function () {
+        this.edit_mode = false;
+        for(let value of editable) {
+          this.profile[value] = this.edits[value];
+        }
       }
     },
     data: function () {
@@ -66,19 +93,30 @@
         edit_mode: false,
         selected_tab: 0,
         personal: this.is_personal(),
-        name: "ramin",
-        "publicName": "ramin",
-        description: "the only hacker around. Comes and say HI!",
-        location: "Barcelona, Spain",
-        "interestedTopics": [],
-        enries: [],
-        following: [],
-        followers: [],
-        groups: [],
-        roles: [],
-        organizations: [],
-        settings: {},
-        interested_topics: ["Climate", "Open Science", "Critical Engineering", "Eastern Philosophy"]
+        profile: {
+          username: "ramin",
+          public_name: "ramin- the dev",
+          description: "the only hacker around. Comes and say HI!",
+          location: "Barcelona, Spain",
+          entries: [],
+          following: [],
+          followers: [],
+          groups: [],
+          roles: [],
+          organizations: [],
+          settings: [],
+          interested_topics: [
+            {name: "Climate", icon: "star"},
+            {name: "Open Science", icon: "room"},
+            {name: "Critical Engineering", icon: "settings"},
+            {name: "Eastern Philosophy", icon: "search"}],
+        },
+        edits: {
+          public_name: "",
+          description: "",
+          location: "",
+          interested_topics: []
+        }
       }
     }
   }
