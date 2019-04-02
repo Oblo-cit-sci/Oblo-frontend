31faa5bf55a48447ebd2dc356f89f2580e2d4542
Ramin S <Ramin.Soleymani@uab.cat>
2019-04-02 12:36:40 +0200

    long text aspect

---
+++ components/aspectInput/TextLong.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div
+    v-textarea("outline" :label="aspect.name"  v-model="value")
+</template>
+
+<script>
+    export default {
+        props: ["aspect"],
+        data() {
+          return {
+            value: ""
+          }
+        },
+        name: "TextLong"
+    }
+</script>
+
+<style scoped>
+
+</style>
