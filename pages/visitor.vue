0187d82a5a6de25c3e2c1d2ee73f12e044a880cb
Ramin <transfluxus@posteo.de>
2019-07-03 02:41:48 +0200

    visitor started import_export module

--- pages/visitor.vue
+++ pages/visitor.vue
@@ -2,26 +2,37 @@
   div
     h2 Welcome visitor
     div {{welcome}}
-      br
-      br
+    br
+    br
     div {{login}}
     v-btn(nuxt to="/login" color="success") Login
     br
     br
     div {{register}}
     v-btn(nuxt to="/register" color="success") Register
-
+    br/
+    br/
+    div {{offline}}
+    br
+    LoadFileButton(@fileload="loaded($event)")
 </template>
 
 <script>
+  import LoadFileButton from "../components/LoadFileButton";
+
   export default {
     name: "visitor",
+    components: {LoadFileButton},
     data() {
       return {
         welcome: "As a visitor you can browse through all public entries and create some entries.",
         login: "If you already have an account, you can login here:",
         register: "If do not have account yet but you want to want all features, including, setting the privacy and licenses of your entries, you can register.",
-        offline:"If you are using this app while being offline, you can import your previously imported profile data into the app. "
+        offline: "If you are using this app while being offline, you can import your previously imported profile data into the app. "
+      }
+    }, methods: {
+      loaded(data) {
+        console.log(data)
       }
     }
   }
