32abf2c754da0f863a0492b94977e60336cfa270
Ramin <transfluxus@posteo.de>
2019-07-22 21:51:15 +0200

    NumberAspect has rules again

--- components/aspectInput/NumberAspect.vue
+++ components/aspectInput/NumberAspect.vue
@@ -11,7 +11,7 @@
       type="number"
       :min="min"
       :max="max"
-      v-on:update:error="error = $event"
+      :rules="[minmax, valid_num_type]"
       :append-outer-icon="clearIcon"
       @click:append-outer="$emit('entryAction', {action: 'clear'})"
       :mask="mask" )
@@ -29,25 +29,32 @@
         mask: "",
         suffix: this.aspect.attr.suffix || "",
         num_type: null,
-        error: false, // todo actually emit it up... and make validation on whole entry...
         min: this.aspect.attr.min,
         max: this.aspect.attr.max,
+        minmax: value => {
+          if (this.aspect.attr.min && value < this.aspect.attr.min)
+            return "value must be higher than " + (this.aspect.attr.min - 1)
+          else if (this.aspect.attr.max && value > this.aspect.attr.max)
+            return "value must be lower than " + (this.aspect.attr.max + 1)
+          else return true
+        },
+        valid_num_type: value => {
+          if (this.num_type === INT && this.i_value % 1 !== 0) {
+            return "value must be an integer"
+          } else
+            return true
+        }
       }
     },
     created() {
       this.num_type = this.aspect.type
-      if(!(this.num_type === INT || this.num_type === FLOAT)) {
+      if (!(this.num_type === INT || this.num_type === FLOAT)) {
         console.log("NumberAspect-error type: aspect, type", this.aspect.name, this.aspect.type)
-      }
-      if (this.aspect.type === INT) {
-        this.mask = "################"
       }
     },
     methods: {
-      input(val){
-        this.value_change(this.num_type === INT ?
-          parseInt(val) :
-          parseFloat(val.replace(",",".")))
+      input(val) {
+        this.value_change(val)
       }
     }
   }
