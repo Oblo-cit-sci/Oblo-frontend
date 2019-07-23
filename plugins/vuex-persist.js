8f95c153b3cbac6d8a8f948e488729ce6b682c22
Ramin <transfluxus@posteo.de>
2019-07-23 22:58:13 +0200

    not sure if that merge worked

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -1,16 +1,6 @@
 import VuexPersistence from 'vuex-persist'
 
 import localforage from 'localforage'
-/*
-export default ({ store }) => {
-  window.onNuxtReady(() => {
-    new VuexPersistence({
-      /!* your options *!/
-      storage: localforage,
-      strictMode: true
-    }).plugin(store);
-  });
-}*/
 
 export default ({ store }) => {
   new VuexPersistence({
