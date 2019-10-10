aa94c67c0de04a8de26ae21049ce93a53b949937
anna <anna.perez@basetis.com>
2019-10-10 08:39:26 +0200

    Creat search component and add it to test page

---
+++ components/Search.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    div
+        v-autocomplete(
+            label="Components"
+            :items="components")
+</template>
+
+<script>
+    export default {
+        name: "Search",
+        props:{},
+        data() {
+            return {
+                components: [
+                    'Autocompletes', 'Comboboxes', 'Forms', 'Inputs', 'Overflow Buttons', 'Selects', 'Selection Controls', 'Sliders', 'Textareas', 'Text Fields',
+                ],
+            }
+        },
+        created() {},
+        mounted() {},
+        methods: {},
+        watch: {},
+        computed: {},
+    }
+</script>
+<style scoped>
+
+</style>
