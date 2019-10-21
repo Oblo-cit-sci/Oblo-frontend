520067628cdcb7e2b91a8aa188da2d21cdc322c2
anna <anna.perez@basetis.com>
2019-10-21 18:32:59 +0200

    Aisle chips in seperate component

--- components/EntryPreview.vue
+++ components/EntryPreview.vue
@@ -4,41 +4,7 @@
       v-col(cols="12" class="col-md-8 col-sm-12 entry-meta")
         div(class="overline") {{entry_date}}
         p(class="headline mb-4") {{entry.title}}
-        div
-          v-chip(v-if="entry.actors.creator.public_name"
-            :ripple="false"
-            small
-            class="mr-2 mb-2"
-            label)
-            v-avatar(left)
-              v-icon(small class="fa fa-user")
-            p(class="ma-0" ) {{entry.actors.creator.public_name}}
-          v-chip(v-if="entry.actors.location"
-            :ripple="false"
-            small
-            class="mr-2 mb-2"
-            label)
-            v-avatar(left)
-              v-icon(small class="fa fa-map-marker")
-            p(class="ma-0" ) {{entry.actors.location}}
-          v-chip(v-if="entry.privacy"
-            :ripple="false"
-            small
-            class="mr-2 mb-2"
-            label)
-            v-avatar(center)
-              v-icon(
-                small) {{privacy_icon(entry.privacy)}}
-          v-chip(v-if="entry.license"
-            :ripple="false"
-            small
-            class="mr-2 mb-2"
-            label) {{entry.license}}
-          v-chip(v-if="entry.license"
-            :ripple="false"
-            small
-            label
-            class="mr-2 mb-2") {{entry.type_slug}}
+        MetaChips
       v-col(v-if="show_image" cols="12" class="col-md-4 col-sm-12 entry-image")
         div(class="float-md-right float-sm-left entry-display-size")
           v-avatar(
@@ -69,20 +35,16 @@
     import {CREATOR, entry_actor_relation} from "../lib/actors";
     import {privacy_icon, printDate} from "../lib/util";
     import {EDIT} from "../lib/consts";
+    import MetaChips from "../components/MetaChips";
 
     export default {
         name: "Entrypreview",
+        components: {MetaChips},
         props: {
             entry: {type: Object, required: true}
         },
         mixins: [EntryNavMixin],
         created() {
-        },
-        data: function () {
-            return {
-              recent: {},
-              date: new Date().toISOString().substr(0, 10)
-            }
         },
         methods: {
             goto(entry) {
@@ -127,7 +89,7 @@
   .custom-card {
     height: 100%
   }
-  
+
   .entry-display-size {
     width: 100%;
   }
