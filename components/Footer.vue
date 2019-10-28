f67e76987e94093e132d938d791447839ebeaee6
anna <anna.perez@basetis.com>
2019-10-28 16:06:35 +0100

    Integrate footer into default layout

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-    v-footer.justify-content-center.custom-footer( 
-      absolute
-      elevation=4)
+    v-footer.justify-content-center.custom-footer(
+      elevation=4
+      app)
         v-col(v-if="!this.$vuetify.breakpoint.sm && !this.$vuetify.breakpoint.xs")
             v-card(flat
                 tile
                 class="white text-center")
-                v-card-text.pb-0
+                v-card-text.pb-0.pt-0
                     v-btn(v-for="item in footer_data"
                         :key="item.logo" class="mx-4 white--text" :href="item.link" target="_blank" text small)
                         v-img(:src="item.logo"  width="70" contain)
@@ -16,7 +16,7 @@
              v-card(flat
                 tile
                 class="white text-center")
-                v-card-text.pb-0
+                v-card-text.pb-0.pt-0
                     v-carousel(height="100" hide-delimiters=true cycle=true)
                         v-carousel-item(v-for="item in footer_data"
                             :key="item.logo")
@@ -73,5 +73,6 @@
     .privacy-policy {
         font-size: 12px;
         padding-top: 8px;
+        padding-bottom: 0px;
     }
 </style>
