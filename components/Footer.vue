adf87ab3be0c575a773ace9506900ebd42f7d73d
anna <anna.perez@basetis.com>
2019-10-28 14:05:31 +0100

    Update footer component and add some footer logos

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,28 @@
 <template lang="pug">
-    v-footer.justify-content-center.custom-footer(dark
-      padless
+    v-footer.justify-content-center.custom-footer( 
       absolute
       elevation=4)
-      v-row(no-gutters)
-        v-col(cols="12" md="10").pa-2
-            span(v-for="item in footer_data" :key="item.item")
-                a(:href="item.link" target="_blank") link
-        v-col(cols="12" md="2").pa-2 
-            a.privacy-policy() {{privacy.title}}
+        v-col(v-if="!this.$vuetify.breakpoint.sm && !this.$vuetify.breakpoint.xs")
+            v-card(flat
+                tile
+                class="white text-center")
+                v-card-text.pb-0
+                    v-btn(v-for="item in footer_data"
+                        :key="item.logo" class="mx-4 white--text" :href="item.link" target="_blank" text small)
+                        v-img(:src="item.logo"  width="70" contain)
+                v-card-text.privacy-policy
+                    a {{privacy.title}}
+        v-col(v-if="this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs")
+             v-card(flat
+                tile
+                class="white text-center")
+                v-card-text.pb-0
+                    v-carousel(height="100" hide-delimiters=true cycle=true)
+                        v-carousel-item(v-for="item in footer_data"
+                            :key="item.logo")
+                            v-img(:src="item.logo" height="60" contain)
+                v-card-text.privacy-policy
+                    a {{privacy.title}}       
 </template>
 
 <script>
@@ -22,11 +36,30 @@
                 },
                 footer_data: [
                     {
-                        logo: 'images/uab-logo.png',
+                        logo: 'images/footer/uab-logo.png',
                         link: 'https://www.uab.cat/web/universitat-autonoma-de-barcelona-1345467954774.html'
+                    },
+                    {
+                        logo: 'images/footer/logo-ICTA-acronim.png',
+                        link: 'https://ictaweb.uab.cat/'
+                    },
+                    {
+                        logo: 'images/footer/2018-LASEG-Logo.png',
+                        link: 'http://icta.uab.cat/Etnoecologia/'
+                    },
+                    {
+                        logo: 'images/footer/LOGO_ERC-FLAG_EU_small.jpg',
+                        link: 'https://erc.europa.eu'
+                    },
+                    {
+                        logo: 'images/footer/Logo_MdM.png',
+                        link: 'https://ictaweb.uab.cat/MdM.php'
                     }
                 ]
             }
+        },
+        mounted () {
+            console.log(this.$vuetify.breakpoint)
         }
     }
 </script>
@@ -38,6 +71,7 @@
         color: black;
     }
     .privacy-policy {
-        font-size: 10px
+        font-size: 12px;
+        padding-top: 8px;
     }
 </style>
