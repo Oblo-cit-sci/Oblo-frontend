e3a906c74e7ce41999f8a7a71a46bacd85ff72d0
Ramin S <Ramin.Soleymani@uab.cat>
2020-02-17 17:00:07 +0100

    add register_login part to domain page

--- pages/domain/_domain/index.vue
+++ pages/domain/_domain/index.vue
@@ -5,7 +5,16 @@
       p.heading {{domain_data.page_index.description}}
     v-divider
     EntryCreateList(
+      v-if="logged_in"
       :entrytypes_entries="entrytypes_entries")
+    div.mt-2(v-else)
+      div.font-weight-bold You have to be registered in order to create entries
+      <!--      div todo same as in pages/index, so make it a small component...-->
+      v-row(align="center")
+        v-col(sm="4" offset="1")
+          v-btn(large to="/register") Register
+        v-col(sm="4")
+          v-btn(large to="/login") Login
     Search(
       :init_clear="true"
       :view_mode.sync="entries_view"
@@ -15,56 +24,59 @@
 
 <script>
 
-    import EntryCreateList from "../../../components/EntryCreateList";
-    import {global_context_filter} from "../../../lib/search";
-    import Search from "../../../components/Search";
-    import {ENTRYTYPES_OF_DOMAIN, DOMAIN, DOMAIN_BY_NAME, SET_DOMAIN} from "../../../lib/store_consts";
-    import {entrytype_filter_options} from "../../../lib/filter_option_consts";
-    import {REGULAR, VIEW_SEARCH} from "../../../lib/consts";
+  import EntryCreateList from "../../../components/EntryCreateList";
+  import {global_context_filter} from "../../../lib/search";
+  import Search from "../../../components/Search";
+  import {ENTRYTYPES_OF_DOMAIN, DOMAIN, DOMAIN_BY_NAME, SET_DOMAIN, USER_LOGGED_IN} from "../../../lib/store_consts";
+  import {entrytype_filter_options} from "../../../lib/filter_option_consts";
+  import {VIEW_SEARCH} from "../../../lib/consts";
 
-    export default {
-        name: "index",
-        components: {EntryCreateList, Search},
-        created() {
-            if (this.domain_data.name !== this.$store.getters[DOMAIN]) {
-                this.$store.commit(SET_DOMAIN, this.domain_data)
-            }
+  import {mapGetters} from "vuex"
+
+  export default {
+    name: "index",
+    components: {EntryCreateList, Search},
+    created() {
+      if (this.domain_data.name !== this.$store.getters[DOMAIN]) {
+        this.$store.commit(SET_DOMAIN, this.domain_data)
+      }
+    },
+    computed: {
+      ...mapGetters({logged_in:USER_LOGGED_IN}),
+      entries_view: {
+        get: function () {
+          return this.$route.query.view || VIEW_SEARCH
         },
-        computed: {
-            entries_view: {
-                get: function () {
-                    return this.$route.query.view || VIEW_SEARCH
-                },
-                set: function (view) {
-                    let route = {
-                        path: this.$route.path,
-                        query: {
-                            view: view,
-                        }
-                    }
-                    this.$router.push(route)
-                }
-            },
-            entrytypes_entries() {
-                return global_context_filter(this.$store.getters[ENTRYTYPES_OF_DOMAIN](this.$route.params.domain))
-            },
-            domain_data() {
-                return this.$store.getters[DOMAIN_BY_NAME](this.$route.params.domain)
-            },
-            filters() {
-                return [entrytype_filter_options]
-            },
-            domain_pre_filter() {
-                return [{
-                    name: "meta_aspect",
-                    meta_aspect_name: DOMAIN,
-                    conditional_value: this.domain_data.name
-                }]
+        set: function (view) {
+          let route = {
+            path: this.$route.path,
+            query: {
+              view: view,
             }
-        },
-        methods: {},
-        watch: {}
-    }
+          }
+          this.$router.push(route)
+        }
+      },
+      entrytypes_entries() {
+        return global_context_filter(this.$store.getters[ENTRYTYPES_OF_DOMAIN](this.$route.params.domain))
+      },
+      domain_data() {
+        return this.$store.getters[DOMAIN_BY_NAME](this.$route.params.domain)
+      },
+      filters() {
+        return [entrytype_filter_options]
+      },
+      domain_pre_filter() {
+        return [{
+          name: "meta_aspect",
+          meta_aspect_name: DOMAIN,
+          conditional_value: this.domain_data.name
+        }]
+      }
+    },
+    methods: {},
+    watch: {}
+  }
 </script>
 
 <style scoped>
