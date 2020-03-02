e11c2190cef6b9bf816edce74acac2120cab574c
Ramin S <Ramin.Soleymani@uab.cat>
2020-03-02 13:05:01 +0100

    persist user data

--- components/actor/LoginMixin.js
+++ components/actor/LoginMixin.js
@@ -7,6 +7,7 @@ export default {
   methods: {
     process_login(login_response_data) {
       this.$store.dispatch(USER_LOGIN, login_response_data)
+      this.persist_user_data()
       this.persist_auth_token()
       this.$axios.setToken("Bearer " + login_response_data.access_token)
     }
