35811f71ff41869266077cae2bd9a28529b5404f
Ramin <transfluxus@posteo.de>
2020-04-08 18:03:53 +0200

    make actor page a static page

--- components/entry/EntryActorList.vue
+++ components/entry/EntryActorList.vue
@@ -1,34 +1,34 @@
 <template lang="pug">
-    div
-        v-chip.ml-1(v-for="actor_role in actors" pill :key="actor_role.actor.registered_name" @click="goto_actor(actor_role.actor)")
-            v-avatar(left)
-                v-img(:src="avatar(actor_role.actor)")
-            span {{actor_role.actor.public_name}}
+  div
+    v-chip.ml-1(v-for="actor_role in actors" pill :key="actor_role.actor.registered_name" @click="goto_actor(actor_role.actor)")
+      v-avatar(left)
+        v-img(:src="avatar(actor_role.actor)")
+      span {{actor_role.actor.public_name}}
 </template>
 
 <script>
-    import {USER_GET_USER_DATA} from "../../store";
+  import {USER_GET_USER_DATA} from "../../store";
 
-    export default {
-        name: "EntryActorList",
-        props: {
-            actors: {
-                type: Array
-            }
-        },
-        methods: {
-            avatar(actor) {
-                return this.$api.url_actor__$registered_name__avatar(actor.registered_name)
-            },
-            goto_actor(actor) {
-                if(actor.registered_name === this.$store.getters[USER_GET_USER_DATA].registered_name){
-                    this.$router.push("/profile")
-                } else {
-                    this.$router.push(`/actor/${actor.registered_name}`)
-                }
-            }
+  export default {
+    name: "EntryActorList",
+    props: {
+      actors: {
+        type: Array
+      }
+    },
+    methods: {
+      avatar(actor) {
+        return this.$api.url_actor__$registered_name__avatar(actor.registered_name)
+      },
+      goto_actor(actor) {
+        if (false && actor.registered_name === this.$store.getters[USER_GET_USER_DATA].registered_name) {
+          this.$router.push("/profile")
+        } else {
+          this.$router.push({path: "/actor", query: {name: actor.registered_name}})
         }
+      }
     }
+  }
 </script>
 
 <style scoped>
