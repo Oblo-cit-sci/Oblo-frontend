f1adcbd8f5b28ca6572398120200c7c8aca09d1a
Ramin <transfluxus@posteo.de>
2020-04-21 20:09:56 +0200

    role icons

--- components/ActorChip.vue
+++ components/ActorChip.vue
@@ -3,6 +3,8 @@
     v-avatar(left)
       v-img(:src="avatar(actor)")
     span(style="user-select:none") {{actor.public_name}}
+    v-icon.pl-1(v-if="role") {{role_icon}}
+
 </template>
 
 <script>
@@ -12,14 +14,31 @@
   export default {
     name: "ActorChip",
     props: {
-      actor: Object
+      actor: Object,
+      role: String
+    },
+    computed: {
+      role_icon() {
+        switch (this.role) {
+          case "creator":
+            return "mdi-file-edit-outline"
+          case "collaborator":
+            return "mdi-pencil"
+          case "shared":
+            return "mdi-eye-outline"
+          default: {
+            console.log("unknown role", this.role)
+            return ""
+          }
+        }
+      }
     },
     methods: {
       avatar(actor) {
         return this.$api.url_actor__$registered_name__avatar(actor.registered_name)
       },
       goto_actor(actor) {
-        if(actor.registered_name === VISITOR)
+        if (actor.registered_name === VISITOR)
           return
         if (actor.registered_name === this.$store.getters[USER_GET_USER_DATA].registered_name) {
           this.$router.push("/profile")
--- components/entry/EntryActorList.vue
+++ components/entry/EntryActorList.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div
-    ActorChip(v-for="actor_role in actors" :actor="actor_role.actor" :key="actor_role.actor.registered_name")
+    ActorChip(v-for="actor_role in actors" :actor="actor_role.actor" :role="actor_role.role" :key="actor_role.actor.registered_name")
 </template>
 
 <script>
@@ -13,8 +13,7 @@
       actors: {
         type: Array
       }
-    },
-
+    }
   }
 </script>
 
