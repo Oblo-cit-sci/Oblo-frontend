bea854a03f6951b6a55f13c0b3583034427a8639
Ramin <transfluxus@posteo.de>
2020-05-20 12:33:51 +0200

    clean some map store stuff

--- store/map.js
+++ store/map.js
@@ -4,44 +4,28 @@ const ld = require("lodash")
 // Mutations
 export const MAP_SET_ENTRIES = "map/set_entries"
 // not used anymore with the new map
-export const MAP_SET_TO_SELECT_ASPECT_LOCATION = "map/set_to_select_aspect_location"
-export const MAP_RESET_TO_SELECT_ASPECT_LOCATION = "map/reset_to_select_aspect_location"
 export const MAP_GOTO_LOCATION = "map/goto_location"  // also getter
 
-// this is used when the selection is not for an aspect (e.g. profile location)
-export const MAP_SELECTED_LOCATION = "map/selected_location"
-export const MAP_GET_SELECTED_LOCATION = "map/get_selected_location"
 // Getters
 export const MAP_LAST_GOTO_LOCATION = "map/last_goto_location"
 // Actions
 export const MAP_GOTO_DONE = "map/goto_done"
-export const MAP_RESET_GOTO_LOCATIONS = "map/reset_goto_locations"
 
 
 export const state = () => ({
-  marker: null,
   entries: {},
-  selected_entry: null,
   goto_location: null,
   last_goto_location: null,
   layers: ["Climate types", "country-label"],
   layer_status: {},
-  to_select_aspect_location: null, // when coming from a locationAspect, comes with
-  selected_location: null // when there is no "to_select_aspect_location" e.g. profile
 })
 
 export const mutations = {
-  marker_point(state, coordinate) {
-    state.marker = coordinate
-  },
   set_entries(state, entries) {
     state.entries = entries
   },
   clear(state) {
     state.entries.clear()
-  },
-  select_entry(state, entry) {
-    state.selected_entry = entry
   },
   goto_location(state, location) {
     state.goto_location = location
@@ -52,24 +36,12 @@ export const mutations = {
   set_layer_status(state, layer_status) {
     state.layer_status = layer_status
   },
-  set_to_select_aspect_location(state, aspect_location) {
-    state.to_select_aspect_location = aspect_location
-  },
-  reset_to_select_aspect_location(state) {
-    state.to_select_aspect_location = null
-  },
-  selected_location(state, location) {
-    state.selected_location = location
-  }
 }
 
 export const getters = {
   entries(state) {
     return state.entries
   },
-  // selected_entry(state) {
-  //   return state.selected_entry
-  // },
   goto_location(state) {
     return () => {
       return state.goto_location
@@ -85,12 +57,6 @@ export const getters = {
   },
   layer_status(state) {
     return state.layer_status
-  },
-  to_select_aspect_location(state) {
-    return state.to_select_aspect_location
-  },
-  get_selected_location(state) {
-    return state.selected_location
   }
 }
 
