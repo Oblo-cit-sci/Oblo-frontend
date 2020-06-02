c6a04bab1ea6e7e176f4d2503fbf027ad5ab01e4
Ramin <transfluxus@posteo.de>
2020-06-02 15:05:51 +0200

    rework on location-aspect

--- components/aspects/LocationAspect.vue
+++ components/aspects/LocationAspect.vue
@@ -20,12 +20,14 @@
               auto-select-first
               v-model="place_select__"
               clearable)
-            span Public location:
-            span
-              v-chip-group(v-if="has_place" active-class="primary--text" mandatory v-model="public_location_precision")
-                v-chip(v-for="(place_part, index) in precision_option" :key="index"
-                  text-color="black"
-                  color="yellow lighten-3") {{place_part}}
+            div(v-if="location_privacy_setting")
+              div Public location:&nbsp;
+                span(v-if="location_privacy_setting === 'randomly moved'") For viewers the exact location is not visible but randomly moved
+            <!--            span-->
+            <!--              v-chip-group(v-if="has_place" active-class="primary&#45;&#45;text" mandatory v-model="public_location_precision")-->
+            <!--                v-chip(v-for="(place_part, index) in precision_option" :key="index"-->
+            <!--                  text-color="black"-->
+            <!--                  color="yellow lighten-3") {{place_part}}-->
     div(v-else)
       span.body-1.readonly-aspect {{place_name}}
       v-btn(v-if="show_goto_button" icon @click="goto_location")
@@ -43,22 +45,17 @@
 <script>
 
   import Mapbox from 'mapbox-gl-vue'
-  import {
-    array2coords,
-    create_location_error,
-    place2str,
-    PREC_OPTION_EXACT_LOC,
-    PREC_OPTION_RANDOM,
-    PREC_OPTION_REGION
-  } from "~/lib/location";
+  import {array2coords, place2str, PREC_OPTIONS_AREA, PREC_OPTIONS_POINT,} from "~/lib/location";
   import SingleSelect from "../input/SingleSelect";
   import {default_place_type} from "~/lib/consts";
   import TriggerSnackbarMixin from "../TriggerSnackbarMixin";
   import AspectComponentMixin from "./AspectComponentMixin";
   import MapIncludeMixin from "~/components/map/MapIncludeMixin"
-  import {context_get_plact_type, convert_to_2d_arr, mapboxgl_lngLat2coords, place_feature2place} from "~/lib/map_utils"
+  import {context_get_place_type, convert_to_2d_arr, mapboxgl_lngLat2coords} from "~/lib/map_utils"
   import GeocodingMixin from "~/components/map/GeocodingMixin"
   import {MAP_GOTO_LOCATION} from "~/store/map"
+  import {USER_SETTINGS} from "~/store/user"
+  import {settings_loc_privacy_activate} from "~/lib/settings"
 
   // "attr.input" options
   const DEVICE = "device"
@@ -81,7 +78,7 @@
         initial_autocomplete_reset: 2, // catch first 2 emit of null: ARGH
         place_part_coordinates: {},
         location_marker: null,
-        btn_loading_search_location: false,
+        btn_loading_search_location: false, // not sure if this is really effective
         search_results: null,
         selected_search_result: undefined, // this because, clear sets it to that too,
         place_select__: null, // this in v-model is only used because of https://github.com/vuetifyjs/vuetify/issues/11383
@@ -90,7 +87,6 @@
       }
     },
     computed: {
-      //  check for attr.input.___
       device_location_input_option() {
         return this.has_input_option(DEVICE)
       },
@@ -105,6 +101,14 @@
       },
       search_location_input_option() {
         return this.has_input_option(SEARCH)
+      },
+      location_privacy_setting() {
+        const location_privacy = this.$store.getters[USER_SETTINGS].location_privacy
+        if (settings_loc_privacy_activate.includes(location_privacy)) {
+          return location_privacy
+        } else {
+          return false
+        }
       },
       // this is for the MapIncludeMixin to show the control
       map_show_geolocate_ctrl() {
@@ -129,9 +133,9 @@
           return options
         }
       },
-      precision_option() {
-        return [PREC_OPTION_EXACT_LOC, PREC_OPTION_RANDOM].concat(this.place_parts)
-      },
+      // precision_option() {
+      //   return [PREC_OPTION_EXACT_LOC, PREC_OPTION_RANDOM].concat(this.place_parts)
+      // },
       //  check for attr.output.___
       has_output_location() {
         return this.has_output(LOCATION)
@@ -145,9 +149,9 @@
         })
       },
       place_name() {
-        // could be just this.has_place
-        if (this.value && this.value.place)
-          return place2str(this.value.place)
+        console.log("place_name", this.value.place_name)
+        if (this.value && this.value.place_name)
+          return {text: this.value.place_name, value: this.value.place_name}
       },
       map_options() {
         if (this.value && this.value.coordinates) {
@@ -169,12 +173,12 @@
     created() {
       // console.log("loc-asp create", this.value)
       if (this.value) {
-        this.search_query = this.place_name
-        this.place_select__ = this.place_name
+        this.search_query = this.value.place_name
+        this.place_select__ = this.value.place_name
         this.search_results = [
           {
-            id: this.place_name,
-            place_name: this.place_name,
+            id: this.value.place_name,
+            place_name: this.value.place_name,
             geometry: {coordinates: convert_to_2d_arr(this.value.coordinates)},
             place_type: this.value.place_type,
             context: this.value.context
@@ -195,6 +199,7 @@
         }
         if ((this.has_output_place)) {
           const place_types = this.aspect.attr.place_types || default_place_type
+          this.btn_loading_search_location = true
           this.rev_geocode(
             {lon: location.coords.longitude, lat: location.coords.latitude},
             {place_types}).then((data) => {
@@ -206,10 +211,13 @@
             this.update_value(value)
           }).catch((err) => {
             console.log("error: mapbox api error", err)
-          }) // must be with else, cuz its async
+          }).finally(() => {
+            setTimeout(() => {
+              this.btn_loading_search_location = false
+            }, 5000)
+          })
         } else {
           this.update_value(value)
-          // this.update_value(value)
         }
       },
       geolocate_error() {
@@ -265,46 +273,86 @@
           place: {}
         }
         if (this.has_output_place) {
-          this.rev_geocode({lon: mapboxEvent.lngLat.lng, lat: mapboxEvent.lngLat.lat}).then(data => {
-            this.querying_location = false
-            if (data.features.length === 0) { // oceans
-              // todo add filler
-              //this.selected_place_text = "No location name"
-            } else {
-              // console.log("fff", data.features)
-              const feature = data.features[0]
-
-              const context = feature.context.map(c => ({
-                text: c.text,
-                place_type: context_get_plact_type(c)
-              }))
-
-              for (let feature of data.features) {
-                const place_type = feature.place_type[0]
-                const context_item = context.filter(c => c.place_type === place_type)
-                if (context_item.length > 0) {
-                  context_item[0].coordinates = array2coords(feature.geometry.coordinates)
-                }
+          const coords = {lon: mapboxEvent.lngLat.lng, lat: mapboxEvent.lngLat.lat}
+          this.rev_geocode(coords).then(data => {
+              console.log("q", data)
+              this.querying_location = false
+              if (data.features.length === 0) { // oceans
+                // todo add filler
+              } else {
+                this.complete_value({
+                  coordinates: coords,
+                  location_precision: PREC_OPTIONS_POINT,
+                }, data.features)
               }
-              const base_place = {
-                text: feature.text,
-                coordinates: array2coords(feature.geometry.coordinates),
-                place_type: feature.place_type[0]
-              }
-              context.push(base_place)
-
-              this.search_query = data.features[0].place_name
-              value.place_type = base_place.place_type
-              value.context = context
-              value.place = place_feature2place(data.features[0])
             }
-          }).catch((err) => {
+          ).catch((err) => {
             console.log("no location found")
             this.querying_location = false
           }).finally(() => {
-            this.update_value(value)
+            // this.update_value(value)
           })
         }
+      },
+      complete_value(value, features) {
+        /*
+        value contains just the coordinates
+        features should have the results of rev-geoquery of the coordinates
+         */
+        // console.log("value", value)
+        // console.log("features", features)
+        if (!value.hasOwnProperty("place")) {
+          value.place = {}
+        }
+        // result from rev-geocoding
+        if (Array.isArray(features)) {
+          for (let place_type of default_place_type) {
+            const place = features.filter(c => c.place_type[0] === place_type)
+            if (place.length > 0) {
+              value.place[place_type] = {name: place[0].text, coordinates: array2coords(place[0].geometry.coordinates)}
+            }
+          }
+        } else {
+          value.place[features.place_type[0]] = {name:features.text, coordinates: value.coordinates}
+          for (let place_type of default_place_type) {
+            for (let context of features.context || []) {
+              const place_type = context_get_place_type(context)
+              value.place[place_type] = {name: context.text}
+            }
+          }
+        }
+        // from selecting one
+        value.place_name = place2str(value.place)
+        console.log("->", value)
+        this.update_value(value)
+        // for (let feature of features) {
+        //   const place_type = feature.place_type[0]
+        //   const context_item = context.filter(c => c.place_type === place_type)
+        //   if (context_item.length > 0) {
+        //     context_item[0].coordinates = array2coords(feature.geometry.coordinates)
+        //   }
+        // }
+
+        //         const data = await this.rev_geocode({lon: coords[0], lat: coords[1]})
+        // console.log(data)
+
+        // const context = feature.context.map(c => ({
+        //   text: c.text,
+        //   place_type: context_get_plact_type(c)
+        // }))
+        //
+
+        // const base_place = {
+        //   text: feature.text,
+        //   coordinates: array2coords(feature.geometry.coordinates),
+        //   place_type: feature.place_type[0]
+        // }
+        // context.push(base_place)
+        //
+        // this.search_query = data.features[0].place_name
+        // value.place_type = base_place.place_type
+        // value.context = context
+        // value.place = place_feature2place(data.features[0])
       },
       update_marker(flyTo = false) {
         const coordinates = this.value.coordinates
@@ -342,37 +390,42 @@
           this.update_value(null)
         } else {
           const feature = this.$_.find(this.search_results, feature => feature.id === sel)
-          const coords = feature.geometry.coordinates
-          const context = feature.context.map(c => ({
-            text: c.text,
-            place_type: context_get_plact_type(c)
-          }))
+          this.complete_value({
+            coordinates: feature.geometry.coordinates,
+            location_precision: PREC_OPTIONS_AREA,
+            // place_type: feature
+          }, feature)
+          console.log("complete")
+          // const coords = feature.geometry.coordinates
+          // const context = feature.context.map(c => ({
+          //   text: c.text,
+          //   place_type: context_get_plact_type(c)
+          // }))
 
-          const data = await this.rev_geocode({lon: coords[0], lat: coords[1]})
 
-          for (let feature of data.features) {
-            console.log(feature)
-            const place_type = feature.place_type[0]
-            const context_item = context.filter(c => c.place_type === place_type)
-            if (context_item.length > 0) {
-              context_item[0].coordinates = array2coords(feature.geometry.coordinates)
-            }
-          }
+          // for (let feature of data.features) {
+          //   console.log(feature)
+          //   const place_type = feature.place_type[0]
+          //   const context_item = context.filter(c => c.place_type === place_type)
+          //   if (context_item.length > 0) {
+          //     context_item[0].coordinates = array2coords(feature.geometry.coordinates)
+          //   }
+          // }
 
-          const base_place = {
-            text: feature.text,
-            coordinates: array2coords(feature.geometry.coordinates),
-            place_type: feature.place_type[0]
-          }
-          context.push(base_place)
-          let value = {
-            coordinates: array2coords(feature.geometry.coordinates),
-            place: place_feature2place(feature),
-            place_type: base_place.place_type,
-            context: context,
-            location_precision: PREC_OPTION_REGION
-          }
-          this.update_value(value)
+          // const base_place = {
+          //   text: feature.text,
+          //   coordinates: array2coords(feature.geometry.coordinates),
+          //   place_type: feature.place_type[0]
+          // }
+          // context.push(base_place)
+          // let value = {
+          //   coordinates: array2coords(feature.geometry.coordinates),
+          //   place: place_feature2place(feature),
+          //   place_type: base_place.place_type,
+          //   context: context,
+          //   location_precision: PREC_OPTION_REGION
+          // }
+          // this.update_value(value)
         }
       },
       complete_value_from_features(value, features) {
@@ -388,9 +441,9 @@
           this.update_marker(true)
         }
         // this when the value comes down as cache /or in whatever way
-        // if(value.place && !this.search_query) {
-        //   this.search_query = place_feature2place(value.place)
-        // }
+        if(value.place_name) {
+          this.search_query = value.place_name
+        }
       },
       map_loaded() {
         if (this.value && this.value.coordinates) {
@@ -400,19 +453,19 @@
       public_location_precision(selection) {
         const option = this.precision_option[selection]
         this.value.location_precision = option
-        if (option === PREC_OPTION_EXACT_LOC) {
-          this.value.public_coordinates = this.value.coordinates
-        } else if (option === PREC_OPTION_RANDOM) {
-          //const location_error = this.$store.getters["user/get_settings"].location_error
-          // todo no magic number
-          this.value.public_coordinates = create_location_error(this.value.coordinates, 2)
-        } else {
-          // console.log(selection, this.value.context)
-          const place_details = this.$_.filter(this.value.context, p => p.text === option)[0]
-          this.value.public_coordinates = place_details.coordinates
-          this.value.location_precision = PREC_OPTION_REGION
-        }
-        this.update_value(this.value)
+        // if (option === PREC_OPTION_EXACT_LOC) {
+        //   this.value.public_coordinates = this.value.coordinates
+        // } else if (option === PREC_OPTION_RANDOM) {
+        //   //const location_error = this.$store.getters["user/get_settings"].location_error
+        //   // todo no magic number
+        //   this.value.public_coordinates = create_location_error(this.value.coordinates, 2)
+        // } else {
+        //   // console.log(selection, this.value.context)
+        //   const place_details = this.$_.filter(this.value.context, p => p.text === option)[0]
+        //   this.value.public_coordinates = place_details.coordinates
+        //   this.value.location_precision = PREC_OPTION_REGION
+        // }
+        // this.update_value(this.value)
       }
     }
   }
