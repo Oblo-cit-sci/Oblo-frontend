672f28945535217c5ea39932ee61e612571d8595
Ramin <transfluxus@posteo.de>
2020-09-01 14:00:00 +0200

    dispatch logout when token is not valid on loading move initialize function into mixin

--- layouts/InitializationMixin.js
+++ layouts/InitializationMixin.js
@@ -5,6 +5,9 @@ import {dev_env} from "~/lib/util"
 import FixDomainMixin from "~/components/global/FixDomainMixin"
 import {PAGE_INDEX} from "~/lib/pages"
 import {default_settings} from "~/lib/settings"
+import {SET_DOMAINS, SET_TEMPLATES_CODES} from "~/store"
+import {USER_GET_AUTH_TOKEN, USER_LOGIN} from "~/store/user"
+import {ENTRIES_HAS_FULL_ENTRY, ENTRIES_SAVE_ENTRY} from "~/store/entries"
 
 export default {
   name: "InitializationMixin",
@@ -32,12 +35,56 @@ export default {
       initialized: APP_INITIALIZED
     }),
   },
+  methods: {
+    async initialize() {
+      this.$store.commit(APP_CONNECTING, true)
+      console.log("initialize")
+
+      const {data} = await this.$api.init_data()
+      const domains_basic_data = data.data.domains
+      this.$store.commit(SET_DOMAINS, domains_basic_data)
+      this.$store.dispatch(SET_TEMPLATES_CODES, data.data.templates_and_codes)
+
+      const auth_token = this.$store.getters[USER_GET_AUTH_TOKEN]
+      if (auth_token.access_token) {
+        const login = await this.$api.actor__validate_token(auth_token)
+        if (login.data.token_valid) {
+          this.$store.commit(USER_LOGIN)
+          this.$api.axios.setToken(auth_token.access_token, "Bearer")
+        } else {
+          console.log("stored token is not valid anymore")
+          // todo, bring this method to the mixin, so we can trigger a snackbar
+          this.$store.dispatch("user/logout")
+          this.$localForage.removeItem("auth_token")
+          this.error_snackbar("You are logged out")
+        }
+      }
+
+      // todo maybe this part should be handled by the individual page, so it can do its default behaviour
+      // but a wrapper would be good.
+      if (this.$route.query.uuid && !store.getters[ENTRIES_HAS_FULL_ENTRY](this.$route.query.uuid)) {
+        console.log("need to get that entry")
+        try {
+          const response = await this.$api.entry__$uuid(this.$route.query.uuid)
+          if (response.status === 200) {
+            this.$store.commit(ENTRIES_SAVE_ENTRY, response.data.data)
+          } else {
+            this.$router.push("/")
+          }
+        } catch (e) {
+          console.log(e)
+          this.$router.push("/")
+        }
+      }
+      return Promise.resolve()
+    }
+  },
   watch: {
     db_loaded(val) {
       // console.log("db loaded", this.initialized)
       if (val) {
         // console.log("layout. initializing")
-        initialize(this.$api, this.$store, this.$route, this.$router, this.$localForage).then(() => {
+        this.initialize().then(() => {
           this.$store.dispatch(APP_CONNECTED)
           // console.log(this.has_multiple_domains, this.get_one_domain_name)
           if (!this.has_multiple_domains) {
