571cefa31a9412a051a0b59d3a7a1cb84c5c74e5
Ramin S <Ramin.Soleymani@uab.cat>
2020-09-28 14:01:30 +0200

    bring back domain specific questions based on language

---
+++ components/domain/DomainLanguageMixin.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "DomainLanguageMixin",
+  methods: {
+    ui_lang_domain_data(domain_name) {
+      return this.lang_domain_data(domain_name, this.$store.getters["user/settings"].ui_language)
+    },
+    lang_domain_data(domain_name, language_code) {
+      const domain_data = this.$store.getters["domain_by_name"](domain_name)
+      return domain_data[language_code]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
