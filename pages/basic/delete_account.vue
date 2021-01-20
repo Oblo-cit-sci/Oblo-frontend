c7a70d7dd55b765625dcaff8f9584f892bcfb8ef
raro <Ramin.Soleymani@uab.cat>
2021-01-20 12:37:00 +0100

    fix account deleting

--- pages/basic/delete_account.vue
+++ pages/basic/delete_account.vue
@@ -16,56 +16,53 @@
 </template>
 
 <script>
-  import Aspect from "~/components/Aspect"
-  import {store_received_entries} from "~/lib/client"
-  import EntryListWrapper from "~/components/EntryListWrapper"
-  import CompactEntryList from "~/components/entry/CompactEntryList"
-  import TriggerSnackbarMixin from "~/components/TriggerSnackbarMixin"
-  import PersistentStorageMixin from "~/components/util/PersistentStorageMixin"
-  import NavBaseMixin from "~/components/NavBaseMixin"
-  import TypicalAspectMixin from "~/components/aspect_utils/TypicalAspectMixin"
+import Aspect from "~/components/Aspect"
+import {store_received_entries} from "~/lib/client"
+import EntryListWrapper from "~/components/EntryListWrapper"
+import CompactEntryList from "~/components/entry/CompactEntryList"
+import TriggerSnackbarMixin from "~/components/TriggerSnackbarMixin"
+import PersistentStorageMixin from "~/components/util/PersistentStorageMixin"
+import NavBaseMixin from "~/components/NavBaseMixin"
+import TypicalAspectMixin from "~/components/aspect_utils/TypicalAspectMixin"
+import {extract_n_unpack_values} from "~/lib/aspect";
 
-  export default {
-    name: "delete_account",
-    mixins: [TriggerSnackbarMixin, PersistentStorageMixin, NavBaseMixin, TypicalAspectMixin],
-    components: {CompactEntryList, EntryListWrapper, Aspect},
-    props: {},
-    data() {
-      return {
-        aspects: {
-          registered_name: this.asp_registered_name(),
-          password: this.asp_password()
-        },
-        entries_to_delete: []
-      }
-    },
-    created() {
-      this.$api.actor.init_delete().then(({data}) => {
-        this.entries_to_delete = store_received_entries(this.$store, data.data)
+export default {
+  name: "delete_account",
+  mixins: [TriggerSnackbarMixin, PersistentStorageMixin, NavBaseMixin, TypicalAspectMixin],
+  components: {CompactEntryList, EntryListWrapper, Aspect},
+  props: {},
+  data() {
+    return {
+      aspects: [this.asp_registered_name(),
+        this.asp_password()
+      ],
+      entries_to_delete: []
+    }
+  },
+  created() {
+    this.$api.actor.init_delete().then(({data}) => {
+      this.entries_to_delete = store_received_entries(this.$store, data.data)
+    })
+  },
+  computed: {
+    any_invalid() {
+      // todo could also have  '|| !a.value'  but we should then be able to pass down the rules to the selectes
+      return this.$_.some(this.aspects, (a) => a.hasOwnProperty("error") && a.error)
+    }
+  },
+  methods: {
+    delete_account() {
+      this.$api.actor.delete_account(extract_n_unpack_values(this.aspects)).then(({data}) => {
+        this.ok_snackbar(data.data)
+        this.clear_storage()
+        this.$store.dispatch("logout")
+        this.home()
+      }).catch(err => {
+        this.err_error_snackbar(err)
       })
-    },
-    computed: {
-      any_invalid() {
-        // todo could also have  '|| !a.value'  but we should then be able to pass down the rules to the selectes
-        return this.$_.some(this.aspects, (a) => a.hasOwnProperty("error") && a.error)
-      }
-    },
-    methods: {
-      delete_account() {
-        this.$api.actor.delete_account({
-          registered_name: this.aspects.registered_name,
-          password: this.aspects.password
-        }).then(({data}) => {
-          this.ok_snackbar(data.data)
-          this.clear_storage()
-          this.$store.dispatch("logout")
-          this.home()
-        }).catch(err => {
-          this.err_error_snackbar(err)
-        })
-      }
     }
   }
+}
 </script>
 
 <style scoped>
