2f6508f5d8f7a4723d3b8e15636afceddea8a01e
raro <Ramin.Soleymani@uab.cat>
2021-02-19 15:18:28 +0100

    translate upload fix

--- pages/translate/translate.vue
+++ pages/translate/translate.vue
@@ -16,38 +16,37 @@
 </template>
 
 <script>
-import {mapGetters} from "vuex";
-import MessageTranslationBlock from "~/components/language/MessageTranslationBlock";
-import SimplePaginate from "~/components/SimplePaginate";
-import TriggerSnackbarMixin from "~/components/TriggerSnackbarMixin";
-import {DOMAIN} from "~/lib/consts";
+import { mapGetters } from 'vuex'
+import MessageTranslationBlock from '~/components/language/MessageTranslationBlock'
+import SimplePaginate from '~/components/SimplePaginate'
+import TriggerSnackbarMixin from '~/components/TriggerSnackbarMixin'
+import { DOMAIN } from '~/lib/consts'
 
 export default {
-  name: "translate",
-  components: {SimplePaginate, MessageTranslationBlock},
+  name: 'Translate',
+  components: { SimplePaginate, MessageTranslationBlock },
   mixins: [TriggerSnackbarMixin],
-  created() {
-    if (!this.setup.dest_lang) {
-      this.$router.push("/translate/setup")
-    }
-  },
   data() {
-    const setup = this.$store.getters["translate/setup_values"]
-    const message_order = setup.messages.map(msg => msg[0])
-    const translations = new Map(setup.messages.map(msg => ([msg[0], {
-      index: msg[0],
-      languages: [setup.src_lang, setup.dest_lang],
-      messages: [msg[1], msg[2]]
-    }])))
-    let translation_o = this.$_.keyBy(setup.messages.map(msg => ({
-      index: msg[0],
-      languages: [setup.src_lang, setup.dest_lang],
-      messages: [msg[1], msg[2]]
-    })), m => m.index)
-
-    console.log(message_order)
-    debugger
-    console.log(translation_o)
+    const setup = this.$store.getters['translate/setup_values']
+    const message_order = setup.messages.map((msg) => msg[0])
+    const translations = new Map(
+      setup.messages.map((msg) => [
+        msg[0],
+        {
+          index: msg[0],
+          languages: [setup.src_lang, setup.dest_lang],
+          messages: [msg[1], msg[2]],
+        },
+      ])
+    )
+    const translation_o = this.$_.keyBy(
+      setup.messages.map((msg) => ({
+        index: msg[0],
+        languages: [setup.src_lang, setup.dest_lang],
+        messages: [msg[1], msg[2]],
+      })),
+      (m) => m.index
+    )
     return {
       show_only_incomplete: false,
       page: 1,
@@ -56,13 +55,18 @@ export default {
       no_changes: true,
       translations,
       message_order,
-      translation_o
+      translation_o,
+    }
+  },
+  created() {
+    if (!this.setup.dest_lang) {
+      this.$router.push('/translate/setup')
     }
   },
   computed: {
-    ...mapGetters({setup: "translate/setup_values"}),
+    ...mapGetters({ setup: 'translate/setup_values' }),
     disable_submit() {
-      return this.no_changes || !this.field_requirements_fulfilled
+      return this.no_changes //|| !this.field_requirements_fulfilled
     },
     // all_messages() {
     //   return this.translations.values()
@@ -75,15 +79,22 @@ export default {
       // let translations = Array.from(this.translations.values())
       let shown_messages = this.message_order
       if (this.show_only_incomplete) {
-        shown_messages = shown_messages.filter(t => ["", null].includes(this.translation_o[t].messages[1]))
+        shown_messages = shown_messages.filter((t) =>
+          ['', null].includes(this.translation_o[t].messages[1])
+        )
       }
-      return shown_messages.slice((this.page - 1) * this.messages_per_page, (this.page) * this.messages_per_page) //translations.slice((this.page - 1) * this.messages_per_page, (this.page) * this.messages_per_page)
+      return shown_messages.slice(
+        (this.page - 1) * this.messages_per_page,
+        this.page * this.messages_per_page
+      ) // translations.slice((this.page - 1) * this.messages_per_page, (this.page) * this.messages_per_page)
     },
     required_messages() {
-      return this.get_required_words().map(w => this.translations.get(w).messages)
+      return this.get_required_words().map(
+        (w) => this.translations.get(w).messages
+      )
     },
     field_requirements_fulfilled() {
-      for (let req_msg of this.required_messages) {
+      for (const req_msg of this.required_messages) {
         console.log(req_msg)
         if (!this.translation_passes(req_msg)) {
           return false
@@ -93,7 +104,7 @@ export default {
     },
   },
   methods: {
-    has_changed({name, change, value}) {
+    has_changed({ name, change, value }) {
       // console.log("msg change", name, change)
       if (change) {
         this.changed_messages.add(name)
@@ -104,52 +115,76 @@ export default {
     },
     async submit() {
       try {
-        if (["fe", "be"].includes(this.setup.component)) {
-          const messages = Array.from(this.changed_messages).map(v => [v, this.translations.get(v).messages[1]])
-          const {data} = await this.$api.language.update_messages(this.setup.component, this.setup.dest_lang, messages)
+        if (['fe', 'be'].includes(this.setup.component)) {
+          const messages = Array.from(this.changed_messages).map((v) => [
+            v,
+            this.translations.get(v).messages[1],
+          ])
+          const { data } = await this.$api.language.update_messages(
+            this.setup.component,
+            this.setup.dest_lang,
+            messages
+          )
           this.ok_snackbar(data.msg)
-          for (let m of messages) {
+          for (const m of messages) {
             this.$refs[m[0]][0].refresh_original()
           }
-        } else if (this.setup.component === "domain") {
+        } else if (this.setup.component === 'domain') {
           const messages = this.get_flat_messages()
+          debugger
           try {
             // todo after the 1. submission, the domain- obj is created, and needs to be patched!
             if (this.setup.config.new_o) {
-              const {data} = await this.$api.domain.post_from_flat(this.setup.config.domain, this.setup.dest_lang, messages)
+              const { data } = await this.$api.domain.post_from_flat(
+                this.setup.config.domain,
+                this.setup.dest_lang,
+                messages
+              )
               this.ok_snackbar(data.msg)
               // todo: this.setup.config.new_o should be changed to false
             } else {
-              const {data} = await this.$api.domain.patch_from_flat(this.setup.config.domain, this.setup.dest_lang, messages)
+              const { data } = await this.$api.domain.patch_from_flat(
+                this.setup.config.domain,
+                this.setup.dest_lang,
+                messages
+              )
               this.ok_snackbar(data.msg)
             }
-            const changed_messages = Object.entries(this.changed_messages)
-            for (let m of this.changed_messages) {
+            // const changed_messages = Object.entries(this.changed_messages)
+            for (const m of this.changed_messages) {
               this.$refs[m][0].refresh_original()
             }
           } catch (e) {
             console.log(e)
             this.err_error_snackbar(e)
           }
-        } else if (this.setup.component === "entries") {
+        } else if (this.setup.component === 'entries') {
           const messages = this.get_flat_messages()
           try {
             if (this.setup.config.new_o) {
-              const {data} = await this.$api.entry.post_from_flat(this.setup.config.entry, this.setup.dest_lang, messages)
+              const { data } = await this.$api.entry.post_from_flat(
+                this.setup.config.entry,
+                this.setup.dest_lang,
+                messages
+              )
               this.ok_snackbar(data.msg)
             } else {
-              const {data} = await this.$api.entry.patch_from_flat(this.setup.config.entry, this.setup.dest_lang, messages)
+              const { data } = await this.$api.entry.patch_from_flat(
+                this.setup.config.entry,
+                this.setup.dest_lang,
+                messages
+              )
               this.ok_snackbar(data.msg)
             }
             const changed_messages = Object.entries(this.changed_messages)
-            for (let m of changed_messages) {
+            for (const m of changed_messages) {
               this.$refs[m[0]][0].refresh_original()
             }
           } catch (e) {
             this.err_error_snackbar(e)
           }
         } else {
-          console.error("Unknown component", this.setup)
+          console.error('Unknown component', this.setup)
         }
       } catch (err) {
         this.err_error_snackbar(err)
@@ -157,54 +192,58 @@ export default {
       }
     },
     back() {
-      this.$router.push("/translate/setup")
+      this.$router.push('/translate/setup')
     },
     get_flat_messages() {
       /**
        * just get the index and dest_msg for all messages (used for domain, entry)
        */
-      return Array.from(this.translations).map(t => [t[0], t[1].messages[1]])
+      return this.message_order.map(m => {
+        const t = this.translation_o[m]
+        return [t.index, t.messages[1]]
+      })
     },
     translation_passes(messages) {
       // console.log(messages[0], messages[1])
-      return messages[0] === "" || messages[1] !== ""
+      return messages[0] === '' || messages[1] !== ''
     },
     is_required(translation_block) {
       return false
     },
     get_required_words() {
       if (this.setup.component === DOMAIN) {
-        return ["title", "description"]
-      } else
-        return []
+        return ['title', 'description']
+      } else return []
     },
     update_msg(index, message) {
-      console.log(index,  message)
+      console.log(index, message)
       this.translation_o[index].messages[1] = message
-    }
+    },
   },
   watch: {
     page(current, prev) {
       setTimeout(() => {
-        this.$vuetify.goTo("body", {offset: 0, duration: 1000, easing: "easeInOutCubic"})
+        this.$vuetify.goTo('body', {
+          offset: 0,
+          duration: 1000,
+          easing: 'easeInOutCubic',
+        })
       }, 50)
     },
     required_messages: {
       deep: true,
       handler(val) {
         console.log(val)
-      }
+      },
     },
     translation_o: {
       deep: true,
-      handler: function (val) {
-        console.log("ta", val)
-      }
-    }
-  }
+      handler(val) {
+        console.log('ta', val)
+      },
+    },
+  },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
