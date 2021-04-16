1080adfe32363f9bb7b9e60b12a228f33eed6f35
raro <Ramin.Soleymani@uab.cat>
2021-04-16 11:24:12 +0200

    little refactor... profile domain-bug not fixed yet experimental feature domain-js plugin load and execution

--- components/tag/TagChip.vue
+++ components/tag/TagChip.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   v-tooltip(bottom v-if="tag.hover" color="rgba(0,0,0,0.9)" open-delay="150")
     template(v-slot:activator="{ on, attrs }")
-      v-chip(class="custom-chip"  outlined  v-bind="attrs" v-on="on") {{tag.text}}
+      v-chip.mr-1(class="custom-chip"  outlined  v-bind="attrs" v-on="on") {{tag.text}}
     div(v-html="hover_content")
-  v-chip(v-else class="custom-chip"  outlined)
+  v-chip.mr-1(v-else class="custom-chip"  outlined)
     v-avatar(left v-if="has_icon")
       v-img(src="")
     span {{tag.text}}
--- pages/Tests.vue
+++ pages/Tests.vue
@@ -7,20 +7,33 @@
 export default {
   name: "Tests",
   mixins: [],
-  components: {
-  },
-  created() {
-    // console.log(this.$api.axios.baseURL)
-  },
-  data() {
+  components: {},
+  head() {
     return {
+      title: "cool",
     }
   },
-  computed: {
-  },
-  methods: {
+  async created() {
+    // experimental feature. load and run domain-js-plugin
+    const url = this.$api.static.url("js/licci.js")
+    const domain_plugin = document.createElement('script')
+    domain_plugin.setAttribute("type", "text/javascript")
+    domain_plugin.setAttribute("src", url)
 
+    domain_plugin.onload = function (d) {
+      console.log("loaded")
+      console.log(d)
+    }
+
+    document.getElementsByTagName("head")[0].appendChild(domain_plugin)
+    const fn = window["crossref_transform"];
+    if (typeof fn === "function") fn({"cool": 4});
   },
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
   watch: {}
 }
 </script>
