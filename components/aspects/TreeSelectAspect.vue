eff84707e7db6876839d0b6f2925ed3502410bf3
Ramin <transfluxus@posteo.de>
2021-06-18 18:46:53 +0200

    minor refactoring

--- components/aspects/TreeSelectAspect.vue
+++ components/aspects/TreeSelectAspect.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div(v-if="!is_view_mode")
+  div(v-if="is_editable_mode")
     LanguageCodeFallback(v-if="!code_entry_language_match" :actual_lang="code_entry_language")
     v-autocomplete(v-if="direct_select && is_empty"
       outlined
