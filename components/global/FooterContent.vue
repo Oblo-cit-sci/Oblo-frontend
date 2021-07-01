16fd995dff45636be5d00b0356ca49bf168cf9ab
raro <Ramin.Soleymani@uab.cat>
2021-07-01 15:25:56 +0200

    refactor..

--- components/global/FooterContent.vue
+++ components/global/FooterContent.vue
@@ -1,28 +1,30 @@
 <template lang="pug">
   v-row
-    v-col(v-if="!this.$vuetify.breakpoint.sm && !this.$vuetify.breakpoint.xs")
+    v-col(v-if="is_mdAndUp")
       v-card.white.text-center(flat tile)
         v-card-text.pb-0.pt-0
           v-btn.mx-4.white--text(v-for="item in footer_data"
             :key="item.logo" :href="item.link" target="_blank" text small)
             v-img(:src="item.logo"  width="70" contain)
-    v-col.py-1(v-if="this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.xs")
+    v-col.py-1(v-else)
       v-card.white.text-center(flat tile)
         v-card-text.pb-0.pt-0
-          v-carousel(height="100%" hide-delimiters=true cycle=true :show-arrows="this.$vuetify.breakpoint.sm")
+          v-carousel(height="100%" hide-delimiters cycle :show-arrows="false")
             v-carousel-item(v-for="item in footer_data" :key="item.logo")
-              v-img(:src="logo_path(item.logo)" height="60" contain)
+              a(:href="item.link" target="_blank")
+                v-img(:src="item.logo" height="60" contain)
 </template>
 
 <script>
 
-import {static_file_path} from "~/lib/util";
 import {NO_DOMAIN} from "~/lib/consts";
+import ResponsivenessMixin from "~/components/ResponsivenessMixin";
 
 const pkg = require('../../package.json')
 
 export default {
   name: "FooterContent",
+  mixins: [ResponsivenessMixin],
   data() {
     return {
       version: pkg.version,
@@ -33,9 +35,6 @@ export default {
     }
   },
   methods: {
-    logo_path(path) {
-      return static_file_path(this.$store, path)
-    }
   },
   computed: {
     footer_data() {
