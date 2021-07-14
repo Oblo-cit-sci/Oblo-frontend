6d09882bd5105ef3d75627c2bd563f55692d23f4
raro <Ramin.Soleymani@uab.cat>
2021-07-14 12:11:30 +0200

    show email address for password reset

--- pages/basic/init_password_reset.vue
+++ pages/basic/init_password_reset.vue
@@ -5,43 +5,47 @@
       :ext_value.sync="query.value"
       mode="edit"
       @update:error="query.error = $event")
-    v-btn(@click='send' color='success' rounded large autofocus :disabled="query.error" :loading="send_button_loading") {{$t('page.init_password_reset.btn_send')}}
+    v-btn(
+      @click='send'
+      color='success'
+      rounded large autofocus
+      :disabled="query.error" :loading="send_button_loading") {{$t('page.init_password_reset.btn_send')}}
     v-alert(:value='errorMsg != null' type='error' prominent transition="scroll-y-reverse-transition") {{errorMsg}}
 </template>
 
 <script>
-  import Aspect from "~/components/Aspect"
-  import TriggerSnackbarMixin from "~/components/TriggerSnackbarMixin"
-  import TypicalAspectMixin from "~/components/aspect_utils/TypicalAspectMixin"
-  import {MSG_PATH_SOMETHING_WENT_WRONG, RESPONSE_ERROR_MSG} from "~/lib/consts"
-  import {unpack} from "~/lib/aspect";
+import Aspect from "~/components/Aspect"
+import TriggerSnackbarMixin from "~/components/TriggerSnackbarMixin"
+import TypicalAspectMixin from "~/components/aspect_utils/TypicalAspectMixin"
+import {MSG_PATH_SOMETHING_WENT_WRONG, RESPONSE_ERROR_MSG} from "~/lib/consts"
+import {unpack} from "~/lib/aspect";
 
-  export default {
-    name: "init_password_reset",
-    components: {Aspect},
-    mixins: [TypicalAspectMixin, TriggerSnackbarMixin],
-    data() {
-      return {
-        query: this.asp_user_query(),
-        errorMsg: null,
-        send_button_loading: false
-      }
-    },
-    methods: {
-      send() {
-        this.send_button_loading = true
-        this.$api.actor.init_password_reset(unpack(this.query.value).toLowerCase()).then(({data}) => {
-          this.ok_snackbar(data.data.msg)
-          this.$router.push("/basic/reset_mail_sent")
-        }).catch(err => {
-          this.errorMsg = this.$_.get(err, RESPONSE_ERROR_MSG, this.$t(MSG_PATH_SOMETHING_WENT_WRONG))
-          setTimeout(() => this.errorMsg = null, 2000)
-        }).finally(() => {
-          this.send_button_loading = false
-        })
-      }
+export default {
+  name: "init_password_reset",
+  components: {Aspect},
+  mixins: [TypicalAspectMixin, TriggerSnackbarMixin],
+  data() {
+    return {
+      query: this.asp_user_query(),
+      errorMsg: null,
+      send_button_loading: false
+    }
+  },
+  methods: {
+    send() {
+      this.send_button_loading = true
+      this.$api.actor.init_password_reset(unpack(this.query.value).toLowerCase()).then(({data}) => {
+        this.ok_snackbar(data.msg)
+        this.$router.push({path:"/basic/reset_mail_sent", query: {address:data.data.address}})
+      }).catch(err => {
+        this.errorMsg = this.$_.get(err, RESPONSE_ERROR_MSG, this.$t(MSG_PATH_SOMETHING_WENT_WRONG))
+        setTimeout(() => this.errorMsg = null, 2000)
+      }).finally(() => {
+        this.send_button_loading = false
+      })
     }
   }
+}
 </script>
 
 <style scoped>
--- pages/basic/reset_mail_sent.vue
+++ pages/basic/reset_mail_sent.vue
@@ -3,15 +3,19 @@
     h3 {{$t("page.reset_mail_sent.h1")}}
     p
       div {{$t("page.reset_mail_sent.p1")}}
+      div.font-weight-bold {{mail_address}}
       div {{$t("page.reset_mail_sent.p2")}}
     div.mt-3
-      nuxt-link(href="/basic/init_password_reset") {{$t("page.reset_mail_sent.a")}}
+      nuxt-link(to="/basic/init_password_reset") {{$t("page.reset_mail_sent.a")}}
 </template>
 
 <script>
   export default {
     name: "reset_mail_sent",
     computed: {
+      mail_address() {
+        return this.$route.query.address
+      }
     }
   }
 
